85aea04c8545b8cd17066ebb67e1b254bd4d9757
Tolani-kitan <omotolanimodupe@gmail.com>
2020-04-15 16:10:57 +0100

    Updates

---
+++ precache-manifest.bc5658150ac826f8d7d963f2ff0e828e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcfb46a5694ab7affb25f55232eaf2dd",
+    "url": "/color-app/index.html"
+  },
+  {
+    "revision": "d376056e3c7b9f3c0165",
+    "url": "/color-app/static/css/main.9151d5a0.chunk.css"
+  },
+  {
+    "revision": "12a987cb1b2b80cc48db",
+    "url": "/color-app/static/js/2.4c94cc3d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/color-app/static/js/2.4c94cc3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d376056e3c7b9f3c0165",
+    "url": "/color-app/static/js/main.a8bdf984.chunk.js"
+  },
+  {
+    "revision": "c0256941dbabf775a34f",
+    "url": "/color-app/static/js/runtime-main.e8ca23f9.js"
+  }
+]);
